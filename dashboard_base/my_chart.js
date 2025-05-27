519bde02131b89054437e4e3122496d9f00de727
ARUHIZA CARLOS <aruhiza87@gmail.com>
2025-05-27 12:22:25 -0400

    dashboard completo

--- dashboard_base/my_chart.js
+++ dashboard_base/my_chart.js
@@ -1,23 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const ctx = document.getElementById('myChart').getContext('2d');
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'],
-            datasets: [{
-                label: 'Ventas por día',
-                data: [150, 220, 180, 300, 250],
-                backgroundColor: 'rgba(201, 121, 0, 0.6)',
-                borderColor: 'rgb(255, 255, 255)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+
+const ctx = document.getElementById('myChart');
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Carlos', 'Fulanito', 'Sutanito', 'Menganito', 'Botanito', 'Tiranito'],
+        datasets: [{
+            label: 'Usuarios Nuevos',
+            data: [3500, 500, 9221, 5431, 2301, 4143],
+            backgroundColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54,162,235,1)',
+                'rgba(255,206,86,1)',
+                'rgba(255,99,132,1)',
+                'rgba(54,162,235,1)',
+                'rgba(255,206,86,1)',
+            ],
+
+        }]
+    },
+    options: {
+        responsive: true,
+
+    }
 });
+
+var earning = document.getElementById('earning');
+var myChart = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+        datasets: [
+            {
+                label: 'VENTAS POR MES',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255,99,132,0.2)',
+                    'rgba(54,162,235,0.2)',
+                    'rgba(255,206,86,0.2)',
+                    'rgba(75,192,192,0.2)',
+                    'rgba(153,102,255,0.2)',
+                    'rgba(255,159,64,0.2)',
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54,162,235,1)',
+                    'rgba(255,206,86,1)',
+                    'rgba(75,192,192,1)',
+                    'rgba(153,102,255,1)',
+                    'rgba(255,159,64,1)',
+                ],
+                borderWith: 1,
+            }]
+    },
+    options: {
+        responsive: true
+    }
+})
